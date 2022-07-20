2810146c1afe8e34e022eb96b5f3cb052a6baf17
McModknower <teetoll@t-online.de>
2022-07-20 09:44:31 +0200

    Fix camera rotation

--- src/camera.cpp
+++ src/camera.cpp
@@ -1,6 +1,5 @@
 #include "camera.h"
 
-#include <iostream>
 #include <cmath>
 
 #include "glut.h"
@@ -8,7 +7,7 @@
 CameraMouseData::CameraMouseData(btScalar distance, Window &window) :
   m_motion_mode(MOTION_MODE_NOTHING),
   m_camera_center_distance(distance),
-  m_camera_transform(btQuaternion(0, 0, 0, 1)),
+  m_camera_transform(btQuaternion(0, 0, 0, 1), btVector3(-3, 0, 0)),
   m_window(window)
 {
   
@@ -18,15 +17,18 @@ void CameraMouseData::startMouseMove(int button, int state, int x, int y) {
   if(state == GLUT_UP) {
     m_motion_mode = MOTION_MODE_NOTHING;
   } else {
+
     btTransform other(btQuaternion(0, 0, 0, 1));
     switch(button) {
     case 3: // WHEEL_UP
       other.setOrigin(btVector3(0.10*m_camera_center_distance, 0.0, 0.0));
       m_camera_transform *= other;
+      m_camera_center_distance *= 0.90;
       break;
     case 4: // WHEEL_DOWN
       other.setOrigin(btVector3((-0.10)*m_camera_center_distance, 0.0, 0.0));
       m_camera_transform *= other;
+      m_camera_center_distance *= 1.10;
       break;
     }
     if(m_motion_mode == MOTION_MODE_NOTHING) {
@@ -51,8 +53,6 @@ void CameraMouseData::updateMouseMove(int x, int y)
   // TODO use width and height instead of 100
   btScalar dx = (x - m_lx) / (btScalar) m_window.m_width;
   btScalar dy = (y - m_ly) / (btScalar) m_window.m_height;
-  btVector3 camera_center(m_camera_center_distance, 0, 0);
-  camera_center = m_camera_transform(camera_center);
   m_lx = x;
   m_ly = y;
 
@@ -61,12 +61,16 @@ void CameraMouseData::updateMouseMove(int x, int y)
     btQuaternion cameraRotation = m_camera_transform.getRotation();
     y_axis = quatRotate(cameraRotation, y_axis);
     
+    btVector3 camera_center(m_camera_center_distance, 0, 0);
+    camera_center = m_camera_transform(camera_center);
+
     btTransform a(btQuaternion(0, 0, 0, 1), camera_center);
 
-    m_camera_transform *= a.inverse();
-    m_camera_transform *= btTransform(btQuaternion(y_axis, dy*M_PI));
-    m_camera_transform *= btTransform(btQuaternion(btVector3(0, 0, 1), dx*M_PI));
-    m_camera_transform *= a;
+    m_camera_transform = a *
+      btTransform(btQuaternion(btVector3(0, 0, 1), dx*M_PI)) *
+      btTransform(btQuaternion(y_axis, dy*M_PI)) *
+      a.inverse() *
+      m_camera_transform;
     
   } else if(m_motion_mode == MOTION_MODE_TRANSLATE) {
     
